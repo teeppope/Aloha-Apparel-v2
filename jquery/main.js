239b31ad35e3ca87e317dddfb43c8204d0d584a7
teeppope <terraepop@gmail.com>
2016-07-15 16:03:18 -0700

    try to add flikity js

---
+++ jquery/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	console.log("hello, I'm your new .js file");
+
+	$('.carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
+});	
+})
--- main.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	console.log("hello, I'm your new .js file");
-	
-})
