246415c5eff2565f5d7a6a26fa746e366fdf5d54
teeppope <terraepop@gmail.com>
2016-07-16 16:23:27 -0700

    add slick slider

--- jquery/main.js
+++ jquery/main.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
 	console.log("hello, I'm your new .js file");
+	
+	//Slick slider jqwererey
+	$('.products-list').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  arrows: true,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1,
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows: false
+	      }
+	    },
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+	});
 
-	$('.carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
-});	
+
 })
