9e25d2858e83eba48a22186054dc474a3f435f52
teeppope <terraepop@gmail.com>
2016-07-17 10:56:19 -0700

    add email validation alert jquerrery

--- jquery/main.js
+++ jquery/main.js
@@ -3,45 +3,65 @@ $(document).ready(function() {
 	
 	//Smooth Scroll jQwerery
 	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+		e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+		var target = this.hash;
+		var $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
 	});
-	
+
 	//Slick slider jqwererey
 	$('.products-list').slick({
-	  dots: true,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 4,
-	  slidesToScroll: 1,
-	  arrows: true,
-	  responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 1,
-	      }
-	    },
-	    {
-	      breakpoint: 600,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1,
-	        arrows: false
-	      }
-	    },
-	  ]
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false
+			}
+		},
+		]
 	});
 
 	//Add to cart button
+
+	//Subscribe elmail allerts
+	function isEmail(email) {
+		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		return regex.test(email);
+	}
+
+	$('.sub-btn').on('click', function(event){
+		event.preventDefault();
+		console.log('Hi, Ive at least been clicked');
+		
+		var email = $('#email').val();
+
+		if (isEmail(email)) {
+			alert('Thanks for subscribing!');
+		}else{
+			alert('Please enter a valid email address')
+			
+		}
+	})
 
 })
